eeb8292f1c846bc4bf1817c529326d2d66488d57
YAN Wenkun <code@yanwk.fun>
2025-06-03 15:02:39 +0800

    First batch

---
+++ zh-cn/1-编译安装.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal
+chcp 65001
+
+@REM 编辑为你的 GPU 对应架构
+@REM 修改时无需保留 "+PTX" ，其用于前向兼容，防止用户忘了该步骤。
+set TORCH_CUDA_ARCH_LIST=6.1+PTX;8.6;8.9;12.0
+
+@REM 使用国内 PyPI 源
+set PIP_INDEX_URL=https://mirrors.cernet.edu.cn/pypi/web/simple
+
+set PATH=%PATH%;%~dp0\MinGit\cmd;%~dp0\python_standalone\Scripts
+
+echo 编译安装 voxelize...
+
+.\python_standalone\python.exe -s -m pip install .\Direct3D-S2\third-party\voxelize
+
+if %errorlevel% neq 0 (
+    echo 编译安装 voxelize 失败！
+    goto :end
+)
+
+echo 编译安装 Direct3D-S2...
+
+.\python_standalone\python.exe -s -m pip install -e .\Direct3D-S2
+
+if %errorlevel% neq 0 (
+    echo 编译安装 Direct3D-S2 失败！
+    goto :end
+)
+
+echo 编译安装完成！
+
+:end
+endlocal
+pause
---
+++ zh-cn/2-下载模型.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+chcp 65001
+
+@REM 如需启用 HF Hub 实验性高速传输，取消该行注释。仅在千兆比特以上网速有意义。
+@REM https://huggingface.co/docs/huggingface_hub/hf_transfer
+rem set HF_HUB_ENABLE_HF_TRANSFER=1
+
+@REM 使用国内镜像站点
+set HF_ENDPOINT=https://hf-mirror.com
+
+@REM 使用国内 PyPI 源
+set PIP_INDEX_URL=https://mirrors.cernet.edu.cn/pypi/web/simple
+
+set HF_HUB_CACHE=%~dp0\HuggingFaceHub
+
+set PATH=%PATH%;%~dp0\MinGit\cmd;%~dp0\python_standalone\Scripts
+
+@REM 重新安装 hf-hub
+if not exist ".\python_standalone\Scripts\.hf-reinstalled" (
+    echo 正在重新安装 huggingface-hub...
+    .\python_standalone\python.exe -s -m pip uninstall --yes huggingface-hub
+    .\python_standalone\python.exe -s -m pip install "huggingface-hub[hf-transfer]"
+    if %errorlevel% equ 0 (
+        echo.> ".\python_standalone\Scripts\.hf-reinstalled"
+    )
+)
+
+echo 正在下载 Direct3D-S2 V1.1 模型...
+
+.\python_standalone\Scripts\huggingface-cli.exe download ^
+"wushuang98/Direct3D-S2" --include "direct3d-s2-v-1-1/*"
+
+echo 脚本执行完毕，如有文件不完整，请重新运行脚本
+
+endlocal
+pause
